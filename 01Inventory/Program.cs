aef5fadadc29bd8b701e3ae6252be0a32fdbcfc3
MsZoezo <zoey@eijgenberger.com>
2021-11-22 14:46:54 +0100

    Opdracht 1 done

---
+++ 01Inventory/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01Inventory
+{
+    class Item
+    {
+        public string name;
+
+        public Item(string name)
+        {
+            this.name = name;
+        }
+    }
+
+    class Inventory
+    {
+        private List<Item> _items = new List<Item>();
+        
+        public void AddItem(Item item)
+        {
+            _items.Add(item);
+        }
+
+        public List<Item> GetItems()
+        {
+            return _items;
+        } 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Inventory inventory = new Inventory();
+
+            inventory.AddItem(new Item("kaas"));
+            inventory.AddItem(new Item("melk"));
+
+            List<Item> items = inventory.GetItems();
+
+            foreach(Item item in items)
+            {
+                Console.WriteLine(item.name);
+            }
+        }
+    }
+}
